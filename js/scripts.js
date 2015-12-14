7394d1e50dbdae4b156dff338a7f957b850e3a70
alyssa horrocks <student@epicodus.com>
2015-12-14 12:32:52 -0800

    Add hover effects on boxes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $(".boxes").hover(function(){
     $(this).find(".project-description").show();
     $(this).find(".letter").hide();
+    $("img").not($(this).find("img")).hide();
     $(this).find("img").css("opacity", "1");
   }, function(){
     $(".project-description").hide();
     $(this).find(".letter").show();
-    $(this).find("img").css("opacity", ".4");
+    $("img").not($(this).find("img")).show();
+    $(this).find("img").css("opacity", ".6");
   });
 
 
