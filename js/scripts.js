671d7b0258cf1f0dfb26ffb616dda7d71930ff7c
alyssa horrocks <student@epicodus.com>
2015-12-14 11:58:02 -0800

    Add jquery to boxes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,15 @@
 $(document).ready(function() {
-
-    $("#slideshow > div:gt(0)").hide();
-
-    setInterval(function() {
-      $('#slideshow > div:first')
-        .hide()
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('#slideshow');
-    }, 3000);
-
-});
+  $(".boxes").hover(function(){
+    $(this).find(".project-description").show();
+    $(this).find(".letter").hide();
+    $(this).find("img").css("opacity", "1");
+  }, function(){
+    $(".project-description").hide();
+    $(this).find(".letter").show();
+    $(this).find("img").css("opacity", ".4");
+  });
 
 
 
 
-
-
-
-
-  // $('#hang-1').hide();
-  // $('#hang-2').hide();
-  // $('#hang-3').hide();
-  //
-  //
-  // $('#box-1').mouseover(function() {
-  //   var active = $('.active');
-  //   slideshow(active);
-  //
-  //   function slideshow(active) {
-  //     if (active.next().attr('class') == "hang") {
-  //       var nextImg = active.next()
-  //     } else {
-  //       var nextImg = $("#hang-1")
-  //     }
-  //
-  //     $(active).fadeIn(1000);
-  //     active.removeClass('active');
-  //     nextImg.addClass('active');
-  //   };
-  // });
+})
