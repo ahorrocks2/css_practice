b0aa2aa37e36b3c8f5d4b9e193b256de1e5abeed
alyssa horrocks <am.rocks@outlook.com>
2015-12-16 15:41:23 -0800

    Move social icons to center channel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
   $(".boxes").not($("#box-6")).hover(function(){
     $(this).find(".project-description").show();
-    $(this).find(".letter").hide();
-    $("img").not($(this).find("img")).hide();
     $(this).find("img").css("opacity", "1");
   }, function(){
     $(".project-description").hide();
-    $(this).find(".letter").show();
-    $("img").not($(this).find("img")).show();
     $(this).find("img").css("opacity", ".1");
   });
 
