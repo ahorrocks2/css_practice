57cb5adef470953a59b81eb457814027cd4ab81e
Alyssa Horrocks <am.rocks@outlook.com>
2015-12-22 16:28:59 -0500

    ...add changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("#about").hide();
-  $("#contact-info").hide();
+  // $("#contact-info").hide();
+  //
+  // $("#about-me-button").click(function() {
+  //   $("#contact-info").hide(), $("#nav-buttons").animate({ top: "100px" }, 1000, function() { $("#about").show(); } );
+  // });
 
   $("#about-me-button").click(function() {
-    $("#contact-info").hide(), $("#nav-buttons").animate({ top: "100px" }, 1000, function() { $("#about").show(); } );
-  });
-
-  $("#contact").click(function() {
-    $("#about").hide(), $("#nav-buttons").animate({ top:"800px" }, 1000, function() { $("#contact-info").show(); } );
+    $("#nav-buttons").animate({ top:"720px" }, 1000, function() { $("#about").show(); } );
   });
 
 
