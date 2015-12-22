b5732ccadc280deac566bcd8c6ddb736f3ab1240
alyssa horrocks <am.rocks@outlook.com>
2015-12-21 16:02:43 -0800

    Change to flexbox.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
-  $(".boxes").not($("#box-6")).hover(function(){
-    $(this).find(".project-description").show();
-    $(this).find("img").css("opacity", "1");
-  }, function(){
-    $(".project-description").hide();
-    $(this).find("img").css("opacity", ".1");
-  });
-
   $("#about").hide();
   $("#contact-info").hide();
 
   $("#about-me-button").click(function() {
-    $("#contact-info").hide(), $("#nav-buttons").animate({ top: "0px" }, 1000, function() { $("#about").show(); } );
+    $("#contact-info").hide(), $("#nav-buttons").animate({ top: "100px" }, 1000, function() { $("#about").show(); } );
   });
 
   $("#contact").click(function() {
-    $("#about").hide(), $("#nav-buttons").animate({ top:"560px" }, 1000, function() { $("#contact-info").show(); } );
+    $("#about").hide(), $("#nav-buttons").animate({ top:"800px" }, 1000, function() { $("#contact-info").show(); } );
   });
 
 
